cc44e639b7d62958ff4d492916d8b17917e1661e
awhogue <ahogue@gmail.com>
2026-01-17 15:07:09 -0500

    Fix requirements

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,7 +5,7 @@ sudo apt install python3.11 python3.11-venv python3.11-dev
 python3.11 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install torch --index-url https://download.pytorch.org/whl/cu121
+pip install "torch>=2.5.0" --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 pip install flash-attn --no-build-isolation
 
